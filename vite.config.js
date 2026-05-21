0f019458396e8922d85dc6fb944ea8f06be0519e
Keisei Higashimura <showchoo0817@gmail.com>
2026-05-21 11:33:14 +0900

    fix css build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 👈 1. これを追加
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // 👈 2. これを追加
-  ],
+  plugins: [react()],
+  base: '/CBA-PUZZLE/',
+  build: {
+    cssMinify: 'esbuild'
+  }
 })
