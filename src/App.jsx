637248bb83489a6c26127b62fbae3d7f17503dce
showchoo <34552117+showchoo@users.noreply.github.com>
2026-05-22 13:52:40 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DynastyView from "./components/DynastyView";
 import { CBACoreEngine } from "./data/cbaEngine";
 import stagesData from "./data/stages.json";
 
+
 const STAGE_IDS = [
   "01","02","03","04","05","06","07","08","09","10","11","12","13",
 ];
